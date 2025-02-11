b97d174ebe6f5e8e1a1a7387ee6864f885219858
hayashit6239 <hyshtmnr6239dcm@gmail.com>
2025-02-12 02:14:04 +0900

    [add] layout の追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,35 +43,3 @@ export default function RootLayout({
         </html>
     );
 }
-
-// const Header2: FC = () => {
-//     return (
-//         <Flex
-//             direction="row"
-//             className="fixed bg-gray-950 bg-gradient-to-b from-gray-950 to-gray-900 w-full h-[80px] px-6 py-8 place-content-between items-center"
-//         >
-//             <Flex direction="row" className="space-x-3 items-center">
-//                 <Link href="/home">
-//                     <Text className="text-red-700 font-bold text-2xl">NextjsAppRouter</Text>
-//                 </Link>
-//                 <Link href="/search">
-//                     <Text className="text-sm">すべて</Text>
-//                 </Link>
-//                 <Link href="/search?category=clothes">
-//                     <Text className="text-sm">トップス</Text>
-//                 </Link>
-//                 <Link href="/search?category=book">
-//                     <Text className="text-sm">本</Text>
-//                 </Link>
-//                 <Link href="/search?category=shoes">
-//                     <Text className="text-sm">シューズ</Text>
-//                 </Link>
-//             </Flex>
-//             <Flex>
-//                 <Button className="font-bold" variant="filled" color="yellow">
-//                     出品
-//                 </Button>
-//             </Flex>
-//         </Flex>
-//     );
-// };
