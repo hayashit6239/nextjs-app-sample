e61585cac3f622a706e7ef66efecd0eae8168920
hayashit6239 <hyshtmnr6239dcm@gmail.com>
2025-02-12 10:29:10 +0900

    [update] 環境変数の調整

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # 概要
 
-xxx という記事のサンプルリポジトリです。
+[Next.js 15 App Router で実践 Web アプリケーション]() という記事のサンプルリポジトリです。
 
 # 利用手順
 
-yyy
+## 0. 想定している動作環境
+
+ローカルで動かすことを想定したリポジトリとなっています。
+
+環境に合わせて [yarn をインストール](https://classic.yarnpkg.com/lang/en/docs/install/#mac-stable)します。
+
+## 1. ビルド
+
+```terminal
+yarn install
+yarn build
+```
+
+下記の出力がでれば成功です。
+
+```
+
+```
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     env: {
-        API_BASE_URL: "http://localhost:8000",
-        ROUTE_HANDLER_BASE_URL: "http://localhost:3000/api",
+        API_BASE_URL: process.env.API_BASE_URL,
+        ROUTE_HANDLER_BASE_URL: process.env.ROUTE_HANDLER_BASE_URL,
     },
     experimental: {
         ppr: true,
